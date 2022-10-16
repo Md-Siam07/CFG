a5ae9e134393c8c9f31ab58e4a051ba2a17de4cc
Md Siam <48762084+Md-Siam07@users.noreply.github.com>
2022-10-16 23:59:18 +0600

    Add files via upload

--- automated_test_case_generator.cpp
+++ automated_test_case_generator.cpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+#define NEG_INF INT32_MIN
+#define POS_INF INT32_MAX
+
 vector<string> lines;
 int curIndex = 0, cyclomaticComplexity=2;
 
@@ -30,6 +33,7 @@ public:
 };
 
 class window{
+public:
     int minn, maxx;
 public:
     window(int mi,int ma){
@@ -40,7 +44,6 @@ public:
 
 class varRanges{
 public:
-
     string var;
     vector<window> windows;
 public:
@@ -55,7 +58,7 @@ string extractVar(string s)
 {
     string tem = "";
     int index  = 0;
-    while(index<s.size() && (s[index]!= '<' &&  s[index]!='>' && s[index]!='=')){
+    while(index<s.size() && (s[index]!= '<' &&  s[index]!='>' && s[index]!='=' && s[index]!='!')){
 
         tem += s[index];
         index++;
@@ -76,10 +79,18 @@ int extractValue(string s)
     return ret;
 }
 
-bool varExist(varRanges v)
+char extractOP(string s){
+    int index  = 0;
+    while(index< s.size() && (s[index]!='<' && s[index]!='>' && s[index]!='=' && s[index]!='!')){
+        index++;
+    }
+    return s[index];
+}
+
+bool varExist(string v, vector<varRanges> vec)
 {
-    for(int i=0; i<vars.size();i++){
-        if(v.var == vars[i].var){
+    for(int i=0; i<vec.size();i++){
+        if(v == vec[i].var){
             return true;
         }
     }
@@ -142,7 +153,7 @@ bool containsLoop(string s)
 
 bool isEnd(string s)
 {
-    return s[s.size()-1]=='}';
+    return s[s.size()-2]=='}';
 }
 
 void readFile(string fileName)
@@ -249,6 +260,7 @@ Node* buildCFG(Node *root, bool isLoop)
         Node* curNode = new Node(curIndex);
 
         if(containsLoop(lines[curIndex]) || containsIf(lines[curIndex])){
+            //cout<< "loop" <<endl;
             cyclomaticComplexity++;
             if(branches.size()>0){
                 for(int i=0;i<branches.size(); i++){
@@ -263,12 +275,20 @@ Node* buildCFG(Node *root, bool isLoop)
             if(containsLoop(lines[curIndex-1]))
             {
                 branches.push_back(curNode);
+                if(containsWhile(lines[curIndex-1])){
+                    string tem = getItemsInsideLoop(lines[curIndex-1]);
+                    //cout << "tem: " << tem << endl;
+                    vector<string> conditions = detachIfMultipleCondition(tem);
+                    vector<char> operators = getOperators(tem);
+                    curNode->operators = operators;
+                    curNode->conditions = conditions;
+                }
                 buildCFG(curNode, true);
             }
             else
             {
                 string tem = getItemsInsideIf(lines[curIndex-1]);
-               // cout << "tem: " << tem << endl;
+                //cout << "tem: " << tem << endl;
                 vector<string> conditions = detachIfMultipleCondition(tem);
                 vector<char> operators = getOperators(tem);
                 curNode->operators = operators;
@@ -303,7 +323,7 @@ Node* buildCFG(Node *root, bool isLoop)
             }
             curIndex++;
             if(isEnd(lines[curIndex-1])){
-
+                //cout << "end" <<endl;
                 if(isLoop){
                     root->hasBackedge = true;
 
@@ -324,19 +344,19 @@ void traverse(Node* current, int level)
 {
     levelNodeCount[level]++;
     visited[current->lineNo] = true;
-    /*for(auto a: current->conditions){
-        cout<< a << " ";
-    }
-    cout<< endl;
-    for(auto a: current->operators){
-        cout<< a << " ";
-    }
-    cout<< endl;*/
-    /*cout<< current->lineNo+1 << endl;
+    // for(auto a: current->conditions){
+    //     cout<< a << " ";
+    // }
+    // cout<< endl;
+    // for(auto a: current->operators){
+    //     cout<< a << " ";
+    // }
+    // cout<< endl;
+    cout<< current->lineNo+1 << endl;
     for( auto a: current->children){
         cout<< a->lineNo+1 << " ";
     }
-    cout<< endl;*/
+    cout<< endl;
     for(int i=0;i< current->children.size(); i++){
         if(!visited[current->children[i]->lineNo]){
             traverse(current->children[i], level+1);
@@ -344,13 +364,154 @@ void traverse(Node* current, int level)
     }
 }
 
-void init()
+void reset()
 {
-
-   /* while(!containsMain(lines[curIndex])){
-        curIndex++;
+    for(int i=0;i<5000;i++){
+        visited[i] = false;
     }
-    curIndex++;*/
+}
+
+window* getWindow(char op, int value, string var){
+    int minn, maxx;
+    if(op == '<'){
+        minn = NEG_INF;
+        maxx = value-1;
+    }else if(op == '>'){
+        minn = value+1;
+        maxx = POS_INF;
+    }
+    else if(op == '='){
+        minn = value;
+        maxx = value;
+    }
+    else if(op == '!'){
+        //cout<< "called" <<endl;
+        bool ff=false;
+        if(varExist(var, vars)){
+            for(int i=0; i<vars.size();i++){
+                if(var == vars[i].var){
+                    for(auto a: vars[i].windows){
+                        cout<< a.minn << " " << a.maxx <<endl;
+                        if(a.minn == NEG_INF){
+                            maxx = POS_INF;
+                            minn = value+1;
+                            ff=true;
+                        }
+                    }
+                }
+                if(!ff){
+                    //cout<< "siam" <<endl;
+                    minn = NEG_INF;
+                    maxx = value-1;
+                }
+            }
+        }
+        else{
+            minn = NEG_INF;
+            maxx = value-1;
+        }
+        
+    }
+    window* toRet = new window(minn, maxx);
+    return toRet;
+}
+
+void prepareVariableWindows(Node* current, int level)
+{
+    //cout<< "c" <<endl;
+    visited[current->lineNo] = true;
+    vector<string> conditions = current->conditions;
+    vector<char> operators = current->operators;
+    int opCount = operators.size();
+    int i,j,leftCount=0, rightCount=0;
+    if(opCount==0) {
+        leftCount = 1; 
+        rightCount = 2;
+    }
+    for(i=0;i<opCount; i++){
+        if(operators[i] == '|') break;
+        leftCount++;
+    }
+    for(j=opCount-1; j>=0;j--){
+        if(operators[j] == '|') break;
+        rightCount++;
+    }
+    cout<< leftCount << " " << rightCount << " " << operators.size() << endl;
+    if(leftCount <= rightCount){
+        cout << "dhuksi" <<endl;
+        for(int i=0; i< leftCount+1 && i<conditions.size(); i++){
+            cout<< conditions[i] <<endl;
+            string var = extractVar(conditions[i]);
+            int value = extractValue(conditions[i]);
+            char op = extractOP(conditions[i]);
+            cout<< var << " " << value << " " << op << endl;
+            window *wn = getWindow(op, value, var);
+            if(varExist(var, vars)){
+                for(int k=0; k<vars.size(); k++){
+                    if(var == vars[k].var){
+                        vars[k].windows.push_back(*wn);
+                    }
+                }
+            }else{
+                varRanges *varR = new varRanges(var);
+                varR->windows.push_back(*wn);
+                cout<< "pushed" <<endl;
+                vars.push_back(*varR);
+            }
+        // window *wn = getWindow()
+        }
+    }
+    else{
+        for(int j=opCount, i=0; i< rightCount + 1 ; i++, j--){ //recheck
+            string var = extractVar(conditions[j]);
+            int value = extractValue(conditions[j]);
+            char op = extractOP(conditions[j]);
+            cout<< var << " " << value << " " << op << endl;
+            window *wn = getWindow(op, value, var);
+            if(varExist(var, vars)){
+                for(int k=0; k<vars.size(); k++){
+                    if(var == vars[k].var){
+                        vars[k].windows.push_back(*wn);
+                    }
+                }
+            }else{
+                varRanges *varR = new varRanges(var);
+                varR->windows.push_back(*wn);
+                cout<< "pushed" <<endl;
+                vars.push_back(*varR);
+            }
+        // window *wn = getWindow()
+        }
+    }
+    // for(auto a: current->conditions){
+    //     cout<< a << " ";
+    // }
+    // cout<< endl;
+    // for(auto a: current->operators){
+    //     cout<< a << " ";
+    // }
+    // cout<< endl;
+    // cout<< current->lineNo+1 << endl;
+    // for( auto a: current->children){
+    //     cout<< a->lineNo+1 << " ";
+    // }
+    // cout<< endl;
+    
+    for(int i=0;i< current->children.size(); i++){
+        //cout<< visited[current->children[i]->lineNo] <<endl;
+        if(!visited[current->children[i]->lineNo]){
+            //cout << "h" <<endl;
+            prepareVariableWindows(current->children[i], level+1);
+        }
+    }
+}
+
+void initStatementCoverage()
+{
+    // while(!containsMain(lines[curIndex])){
+    //     curIndex++;
+    // }
+    // curIndex++;
     Node *root = new Node(curIndex);
     curIndex++;
     buildCFG(root, false);
@@ -358,12 +519,58 @@ void init()
     s[23] = '0' + cyclomaticComplexity ;
     traverse(root, 0);
     cout<< "cyclomatic complexity: " << cyclomaticComplexity << endl;
+    reset();
+    
+    prepareVariableWindows(root, 0);
+    cout<< "varsize: " << vars.size() <<endl;
+    for(auto a: vars){
+        cout << "var: " << a.var << endl;
+        cout << "windows: ";
+        for(auto b: a.windows){
+            cout<< b.minn << " " << b.maxx << endl;
+        }
+    }
 }
+
+void statementCoverage()
+{
+
+}
+
+void menu()
+{
+    while(1)
+    {
+        lines.clear();
+        curIndex = 0;
+        vars.clear();
+        cout << "1. Statement Coverage\n2. Branch Coverage\n3. Condition Coverage\n4. Visualize CFG\n";
+        int choice;
+        cin>> choice;
+        cout << "Enter file name: ";
+        string filename;
+        cin >> filename;
+        readFile(filename);
+        if(choice == 1){
+            initStatementCoverage();
+            statementCoverage();
+        }
+        else if(choice == 2){
+            
+        }
+        else if(choice == 3){
+            
+        }
+        else if(choice == 4){
+            
+        }
+    }
+}
+
 
 int main()
 {
-    readFile("stack_using_array.cpp");
-    init();
+    menu();
     /*string in =  getItemsInsideElseIf("else if(a>b && b>c || c<a)");
     vector<string> detached = detachIfMultipleCondition(in);
     for(auto a:detached){
@@ -373,9 +580,8 @@ int main()
     for(auto a:operators){
         cout<< a<< endl;
     }*/
-
-    string s = getItemsInsideLoop("while(siam<51)");
-    cout << extractVar(s) <<endl;
-    cout << extractValue(s) << endl;
 }
 
+
+//traverse the graph again with bfs
+//create else node if no else exist
